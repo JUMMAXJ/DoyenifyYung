e348d0365d0dc4dba01b407fbba09ecf60c7632f
JUMMAXJ <adeoyeoluwatoyin12@gmail.com>
2026-03-24 11:18:55 +0200

    Commit for the YungDoyen Registration

---
+++ cypress/e2e/Regspec.cy.js
@@ -0,0 +1,54 @@
+///<reference types= "cypress"/>
+import { RegistrationPage } from "../Pages/RegPageObject.js";
+
+describe("Registration Test", () => {
+  const user = Cypress.env("users").validUser;
+
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("for Registration Page", () => {
+    const Email = "beeneing1944@armyspy.com";
+    const firstName = "Joy";
+    const lastName = "Peace";
+    const Country = "Estonia";
+    const PhoneNumber = "+37257850134";
+    const Password = "Joy12345";
+    const ConfirmPassword = "Joy12345";
+    const Password1 = "Peacejoy*1";
+    const ConfirmPassword1 = "Peacejoy*1";
+    const Month = "January";
+    const Day = "1";
+    const Year = "2015";
+    const Username = "Israel";
+
+    RegistrationPage.getLogo().click();
+    RegistrationPage.getSignUp().click();
+    RegistrationPage.getParent().click();
+    RegistrationPage.getSignUpWithEmail().click();
+    RegistrationPage.getFillEmail().type(Email);
+    RegistrationPage.getFillFirstName().type(firstName);
+    RegistrationPage.getFillLastName().type(lastName);
+    RegistrationPage.getSelectCountry().select(Country);
+    RegistrationPage.getFillPhoneNumber().type(PhoneNumber);
+    RegistrationPage.getFillPassword().type(Password);
+    RegistrationPage.getFillConfirmPassword().type(ConfirmPassword);
+    RegistrationPage.getSubmit().click();
+    RegistrationPage.getLearner().click();
+    RegistrationPage.getFillMonth().select(Month);
+    RegistrationPage.getFillDay().select(Day);
+    RegistrationPage.getFillYear().select(Year);
+    RegistrationPage.getNextDOB().click();
+    RegistrationPage.getFillParentEmail().type(Email);
+    RegistrationPage.getNextParent().click();
+    RegistrationPage.getFillUserName().type(Username);
+    RegistrationPage.getNextUsername().click();
+    RegistrationPage.getFillPassword1().type(Password1);
+    RegistrationPage.getFillConfirmPassword1().type(ConfirmPassword1);
+    RegistrationPage.getCreateAccount().click();
+    RegistrationPage.getAssertion();
+    RegistrationPage.getUrl();
+    RegistrationPage.getSignUpAssertion();
+  });
+});
